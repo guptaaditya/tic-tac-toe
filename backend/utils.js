30c18c3890f6692e9137623f11a517e34bdf8092
Aditya Gupta <guptaaditya24@gmail.com>
2020-03-23 02:47:48 +0530

    winner logic moved to backend, so that it isn't played around on frontend.

--- backend/utils.js
+++ backend/utils.js
@@ -1,6 +1,5 @@
-import _ from 'lodash';
-
-export function getWinner(boxes) {
+const _ = require('lodash');
+function getWinner(boxes) {
     const winningRows = [
         [0, 1, 2],
         [3, 4, 5],
@@ -10,17 +9,20 @@ export function getWinner(boxes) {
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6],
-      ];
-
-      let winner = null;
-      _.find(winningRows, row => {
+    ];
+  
+    let winner = null;
+    _.find(winningRows, row => {
         const [i, ii, iii] = row;
         if (boxes[i] && boxes[ii] && boxes[i] === boxes[ii] && boxes[ii]=== boxes[iii]) {
             winner = boxes[i];
             return true;
         }
         return false;
-      });
-
-      return winner;
+    });
+  
+    return winner;
+}
+module.exports = {
+    getWinner,
 }
